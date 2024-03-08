f9bf2c0fcf0f224bdc3c2514c440238cc9075909
Celine Liu <mangolzj@gmail.com>
2024-03-08 11:41:03 -0500

    added the dotenv library

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`listening on port ,' ${PORT}`);
