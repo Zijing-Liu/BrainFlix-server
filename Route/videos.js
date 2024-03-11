d2e59b63b782f577fba23cd8dd93b30d1e2e7e1e
Celine Liu <mangolzj@gmail.com>
2024-03-10 23:11:40 -0400

    used node fs to handle file read and write, needs to address the issue of rewriting exisiting data with a post

--- Route/videos.js
+++ Route/videos.js
@@ -1,11 +1,11 @@
-const videos = require("../data/videos.json");
-const videoDetails = require("../data/video-details.json");
 const express = require("express");
 const router = express.Router();
 const uniqid = require("uniqid");
 const placeholderImg = "/images/Upload-video-preview.jpg";
 const placeholderChannel = "Dalia Bennu";
-
+const fs = require("fs");
+const videosPath = "./data/videos.json";
+// route with method of getting the video details with an id
 router.get("/:id", (req, res) => {
   const videoDetailWithId = videoDetails.find((vd) => vd.id === req.params.id);
   if (videoDetailWithId) {
@@ -14,23 +14,50 @@ router.get("/:id", (req, res) => {
     res.status(404).json({ error: "Video not found" });
   }
 });
-
-router.get("/", (req, res) => {
-  res.json({ data: videos });
+// get all videos
+router.get("/", async (req, res) => {
+  fs.readFile(videosPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading data file");
+      return;
+    }
+    res.json({ data: JSON.parse(data) });
+  });
 });
 
+// upload a new video
 router.post("/", (req, res) => {
+  let currentVideos = [];
+  // read the existing file
+  fs.readFile(videosPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading data file");
+      return;
+    } else {
+      console.log(data);
+      currentVideos = data;
+    }
+  });
+  console.log("should not be empty", currentVideos);
+
   const newVideo = {
     id: uniqid(),
     title: req.title,
     channel: placeholderChannel,
     image: placeholderImg,
   };
-  videos.push(newVideo);
-  res.json({ status: "success", message: "request recieved" }), 200;
+  currentVideos.push(newVideo);
+  console.log(currentVideos);
+  fs.writeFile(videosPath, JSON.stringify(currentVideos), (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error uploading a video");
+      resturn;
+    }
+    res.send("Video upload success");
+  });
 });
-// router.post(":id/comments", (req, res) => {
-//   res.json({ message: "success" });
-// });
 
 module.exports = router;
