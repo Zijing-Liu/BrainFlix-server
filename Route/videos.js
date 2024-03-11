a0709ab7e4bf7df9c2aaa087ed2be8692119cccf
Celine Liu <mangolzj@gmail.com>
2024-03-11 10:24:27 -0400

    rewrote the post methods to append new data to the end of current data array before rewriting to json file

--- Route/videos.js
+++ Route/videos.js
@@ -5,15 +5,7 @@ const placeholderImg = "/images/Upload-video-preview.jpg";
 const placeholderChannel = "Dalia Bennu";
 const fs = require("fs");
 const videosPath = "./data/videos.json";
-// route with method of getting the video details with an id
-router.get("/:id", (req, res) => {
-  const videoDetailWithId = videoDetails.find((vd) => vd.id === req.params.id);
-  if (videoDetailWithId) {
-    res.json({ data: videoDetailWithId });
-  } else {
-    res.status(404).json({ error: "Video not found" });
-  }
-});
+
 // get all videos
 router.get("/", async (req, res) => {
   fs.readFile(videosPath, "utf8", (err, data) => {
@@ -26,37 +18,58 @@ router.get("/", async (req, res) => {
   });
 });
 
+// route with method of getting the video details with an id
+router.get("/:id", (req, res) => {
+  const videoDetailWithId = videoDetails.find((vd) => vd.id === req.params.id);
+  if (videoDetailWithId) {
+    res.json({ data: videoDetailWithId });
+  } else {
+    res.status(404).json({ error: "Video not found" });
+  }
+});
+
 // upload a new video
 router.post("/", (req, res) => {
-  let currentVideos = [];
   // read the existing file
   fs.readFile(videosPath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Error reading data file");
+      res.send("err");
       return;
-    } else {
-      console.log(data);
-      currentVideos = data;
     }
-  });
-  console.log("should not be empty", currentVideos);
+    let currentVideos = [];
+    try {
+      // parse the existing video data as JSON
+      currentVideos = JSON.parse(data);
+    } catch (parseError) {
+      console.error("Error parsing JSON data:", parseError);
+      res.status(500).send("Error parsing data file");
+      return;
+    }
 
-  const newVideo = {
-    id: uniqid(),
-    title: req.title,
-    channel: placeholderChannel,
-    image: placeholderImg,
-  };
-  currentVideos.push(newVideo);
-  console.log(currentVideos);
-  fs.writeFile(videosPath, JSON.stringify(currentVideos), (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error uploading a video");
-      resturn;
-    }
-    res.send("Video upload success");
+    const newVideo = {
+      id: uniqid(),
+      title: req.body.title,
+      channel: placeholderChannel,
+      image: placeholderImg,
+    };
+
+    // Add the new video to the list of current videos
+    currentVideos.push(newVideo);
+
+    // Write the updated list of videos back to the file
+    fs.writeFile(
+      videosPath,
+      JSON.stringify(currentVideos, null, 2),
+      (writeErr) => {
+        if (writeErr) {
+          console.error(writeErr);
+          res.status(500).send("Error uploading a video");
+          return;
+        }
+        res.send("Video upload success");
+      }
+    );
   });
 });
 
